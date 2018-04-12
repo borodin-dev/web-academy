50dc06164488497f8df3e9f8ab7f6f3ed7c46c6a
borodin-dev <borodin.dev@gmail.com>
2018-04-12 15:39:32 +0300

    index, js, css files

---
+++ src/index/scripts/activateTrafficLight.js
@@ -0,0 +1,21 @@
+// function redLight() {
+//     // document.getElementsByClassName('reds').onclick = function () {
+//     //     document.getElementsByClassName('reds').style.background = 'red';
+//     // }
+//     // var anchors = document.getElementsByClassName('.reds');
+//     // anchors.onclick = function() {
+//     //     alert('ho ho ho');
+//     // }
+// }
+function activateTrafLight() {
+    alert('bimba');
+    // document.getElementById('activate').onclick = function() {
+    //     document.getElementById('traffic-light__lamp').removeAttribute(disabled);
+    // }
+    console.log('func');
+}
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     //do work
+// });
+console.log('text');
